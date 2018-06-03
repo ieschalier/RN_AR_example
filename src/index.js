d99a8c52bebfc4826890ca064bc079b736fd6413
Igor ESCHALIER <igor.eschalier@gmail.com>
2018-06-03 23:18:08 +0200

    Display aircraft

--- src/index.js
+++ src/index.js
@@ -1,35 +1,86 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { GLView } from 'expo'
-import * as THREE from 'three'
+import { GLView, Asset, FileSystem } from 'expo'
+import '@expo/browser-polyfill'
+
+const THREE = require('three')
+global.THREE = THREE
+require('../lib/ObjLoader')
+require('../lib/MTLLoader')
 import ExpoTHREE from 'expo-three'
 
 console.disableYellowBox = true
 
 export default class App extends React.Component {
-  handleContextCreate = gl => {
+  state = {
+    loaded: false,
+  }
+
+  componentWillMount = () => {
+    this.preloadAssetsAsync()
+  }
+
+  preloadAssetsAsync = async () => {
+    await Promise.all(
+      [
+        require('./assets/textures/E-45_col.jpg'),
+        require('./assets/textures/E-45-nor_1.jpg'),
+        require('./assets/textures/E-45_glass_nor_.jpg'),
+        require('./assets/aircraft.obj'),
+        require('./assets/aircraft.mtl'),
+        require('./assets/textures/E-45-nor_1.jpg'),
+      ].map(module => Asset.fromModule(module).downloadAsync()),
+    )
+    this.setState({ loaded: true })
+  }
+
+  handleContextCreate = async gl => {
+    const arSession = await this.glView.startARSessionAsync()
+
     const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      gl.drawingBufferWidth / gl.drawingBufferHeight,
-      0.1,
+    const camera = ExpoTHREE.createARCamera(
+      arSession,
+      gl.drawingBufferWidth,
+      gl.drawingBufferHeight,
+      0.01,
       1000,
     )
 
     const renderer = ExpoTHREE.createRenderer({ gl })
+    scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer)
     renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight)
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-    const cube = new THREE.Mesh(geometry, material)
-    scene.add(cube)
+    // lights
+    const dirLight = new THREE.DirectionalLight(0xffffff, 0.5)
+    dirLight.position.set(0.4, 1, 0)
+    scene.add(dirLight)
+    const ambLight = new THREE.AmbientLight(0x606060)
+    scene.add(ambLight)
 
-    camera.position.z = 5
+    // aircraft model
+    const mtlLoader = new THREE.MTLLoader()
+    const aircraftMTLAsset = Asset.fromModule(require('./assets/aircraft.mtl'))
+    await aircraftMTLAsset.downloadAsync()
+    const mtlMaterials = mtlLoader.parse(
+      await FileSystem.readAsStringAsync(aircraftMTLAsset.localUri),
+    )
+
+    mtlMaterials.preload()
+
+    const aircraftAsset = Asset.fromModule(require('./assets/aircraft.obj'))
+    await aircraftAsset.downloadAsync()
+    const aircraftLoader = new THREE.OBJLoader()
+    aircraftLoader.setMaterials(mtlMaterials)
+    const aircraft = aircraftLoader.parse(
+      await FileSystem.readAsStringAsync(aircraftAsset.localUri),
+    )
+    aircraft.position.z = -10
+    aircraft.rotation.y = 90
+    scene.add(aircraft)
 
     const animate = () => {
       requestAnimationFrame(animate)
-      cube.rotation.x += 0.07
-      cube.rotation.y += 0.04
+      aircraft.rotation.y += 0.03
       renderer.render(scene, camera)
       gl.endFrameEXP()
     }
@@ -37,8 +88,15 @@ export default class App extends React.Component {
   }
 
   render() {
+    const { loaded } = this.state
     return (
-      <GLView onContextCreate={this.handleContextCreate} style={{ flex: 1 }} />
+      loaded && (
+        <GLView
+          ref={ref => (this.glView = ref)}
+          onContextCreate={this.handleContextCreate}
+          style={{ flex: 1 }}
+        />
+      )
     )
   }
 }
