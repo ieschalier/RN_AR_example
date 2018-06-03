0b491867f1d212926f8e2feaeccd094ee2785fa2
Igor ESCHALIER <igor.eschalier@gmail.com>
2018-06-03 23:30:18 +0200

    Rotate aircraft with finger

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, PanResponder } from 'react-native'
 import { GLView, Asset, FileSystem } from 'expo'
 import '@expo/browser-polyfill'
 
@@ -18,6 +18,41 @@ export default class App extends React.Component {
 
   componentWillMount = () => {
     this.preloadAssetsAsync()
+    this.panResponder = PanResponder.create({
+      // Ask to be the responder:
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        // The gesture has started. Show visual feedback so the user knows
+        // what is happening!
+        // gestureState.d{x,y} will be set to zero now
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        // The most recent move distance is gestureState.move{X,Y}
+        // The accumulated gesture distance since becoming responder is
+        // gestureState.d{x,y}
+        if (gestureState.numberActiveTouches === 1) {
+          this.aircraft.rotation.y += gestureState.dx * 0.001
+        }
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderRelease: (evt, gestureState) => {
+        // The user has released all touches while this view is the
+        // responder. This typically means a gesture has succeeded
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        // Another component has become the responder, so this gesture
+        // should be cancelled
+      },
+      onShouldBlockNativeResponder: (evt, gestureState) => {
+        // Returns whether this component should block native components from becoming the JS
+        // responder. Returns true by default. Is currently only supported on android.
+        return true
+      },
+    })
   }
 
   preloadAssetsAsync = async () => {
@@ -71,16 +106,17 @@ export default class App extends React.Component {
     await aircraftAsset.downloadAsync()
     const aircraftLoader = new THREE.OBJLoader()
     aircraftLoader.setMaterials(mtlMaterials)
-    const aircraft = aircraftLoader.parse(
+    this.aircraft = aircraftLoader.parse(
       await FileSystem.readAsStringAsync(aircraftAsset.localUri),
     )
-    aircraft.position.z = -10
-    aircraft.rotation.y = 90
-    scene.add(aircraft)
+    this.aircraft.position.z = -10
+    this.aircraft.position.y = -3
+    this.aircraft.rotation.y = 90
+    scene.add(this.aircraft)
 
     const animate = () => {
       requestAnimationFrame(animate)
-      aircraft.rotation.y += 0.03
+      // this.aircraft.rotation.y += 0.03
       renderer.render(scene, camera)
       gl.endFrameEXP()
     }
@@ -92,6 +128,7 @@ export default class App extends React.Component {
     return (
       loaded && (
         <GLView
+          {...this.panResponder.panHandlers}
           ref={ref => (this.glView = ref)}
           onContextCreate={this.handleContextCreate}
           style={{ flex: 1 }}
